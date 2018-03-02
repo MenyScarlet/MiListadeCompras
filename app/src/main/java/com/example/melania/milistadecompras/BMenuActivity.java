cab991a820ce57900ad0b90f53b5d186bd9f8d97
Melania <melania.gallego.22@gmail.com>
2018-03-02 13:30:53 +0100

    seguir con el ListView

---
+++ app/src/main/java/com/example/melania/milistadecompras/BMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.melania.milistadecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmenu);
+    }
+}
