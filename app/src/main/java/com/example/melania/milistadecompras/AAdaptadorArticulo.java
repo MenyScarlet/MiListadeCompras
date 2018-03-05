3e76fe6bb385d059e42bd8c1fbfaf3ef78c49e72
Melania <melania.gallego.22@gmail.com>
2018-03-05 18:47:31 +0100

    seguir con el ListView

--- app/src/main/java/com/example/melania/milistadecompras/AAdaptadorArticulo.java
+++ app/src/main/java/com/example/melania/milistadecompras/AAdaptadorArticulo.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
@@ -46,6 +47,10 @@ public class AAdaptadorArticulo extends ArrayAdapter<AArticulo> {
         TextView tvPrecio = (TextView)
                 item.findViewById(R.id.LcTvPrecio);
         precio = Double.parseDouble(tvPrecio.getText().toString());
+
+        CheckBox cbProducto = (CheckBox)
+                item.findViewById(R.id.LcCbProducto);
+        //cbProducto.setChecked(articulos.isChequed();
 
         return item;
     }
